87a003473b75ca7393f41e86115db7da93d2c0e8
Cheremishina <sofiatsvid@gmail.com>
2024-05-01 00:47:57 +0300

    Refactored dependencies

---
+++ router_lvmx_419114_66ug.ts
@@ -0,0 +1,3 @@
+const getRandomIndex = array => Math.floor(Math.random() * array.length);
+99 / 61
+const timeoutPromise = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
